78610d94086ee4cee21e938eed451535df05b436
shilad <shilad@gmail.com>
2015-03-27 23:15:03 -0500

    added in a working version of Dave's code.

--- py/whois.py
+++ py/whois.py
@@ -1,3 +1,22 @@
+"""
+
+A module that extracts countries from whois records.
+Adapted by Shilad Sen from ruby code developed by Dave Musicant.
+
+There are two strategies used to extract whois records:
+
+1. Parsed extractions, where the structure of a whois record is
+analyzed and the administrative country is extracted from the
+appropriate field.
+
+2. Freetext extractions, where a whois record is scanned for known
+aliases to countries.
+
+The parsed strategy is always favored, and the freetext strategy is
+only used as a last resort.
+
+"""
+
 import collections
 import os
 import re
@@ -8,24 +27,30 @@ from gputils import *
 
 from country import read_countries
 
-class OfflineWhoisProvider:
-    def __init__(self, path=None):
-        if not path: path = get_feature_data_path('whois')
-        if not os.path.isfile(path):
-            raise GPException('whois results not available.')
+class WhoisProvider:
+    """
+        A provided that resolves countries associated with a whois record.
+        A cache is kept so that whois records are only queried once.
+    """
+    def __init__(self, cache_path=None):
+        self.cache_path = cache_path
+        if not self.cache_path:
+            self.cache_path = get_feature_data_path('whois')
+        if not os.path.isfile(self.cache_path):
+            raise GPException('whois cache %s does not exist.' % self.cache_path)
 
         warn('reading whois results...')
         nlines = 0
-        self.parsed = {}
-        self.freetext = {}
-        for line in gp_open(path):
+        self.cache = {}
+        f = gp_open(self.cache_path)
+        for line in f:
             tokens = line.strip().split('\t')
             if len(tokens) == 2:
                 domain = tokens[0]
                 whois = tokens[1]
                 if whois.endswith('|p'):
                     country = whois[:-2]
-                    if country != '??': self.parsed[domain] = country
+                    if country != '??': self.cache[domain] = country
                 else:
                     dist = {}
                     for pair in whois.split(','):
@@ -34,21 +59,67 @@ class OfflineWhoisProvider:
                     total = 1.0 * sum(dist.values())
                     if sum > 0:
                         for c in dist: dist[c] /= total
-                        self.freetext[domain] = dist
+                        self.cache[domain] = dist
                 nlines += 1
             else:
                 warn('invalid whois line: %s' % `line`)
         warn('finished reading %d whois entries' % nlines)
+        f.close()
+
+        self.countries = read_countries()
+        self.aliases = read_aliases()
+        self.regexes = build_regexes(self.aliases)
 
     def getParsed(self, url):
-        return self.parsed.get(url2registereddomain(url))
+        """
+        Retrieves the country code associated with a URL using the structured
+        strategy, or returns None if it does not succeed.
+        """
+        d = url2registereddomain(url)
+        if not d:
+            return None
+        if d not in self.cache:
+            self.add_to_cache(d)
+        if d not in self.cache or type(self.cache[d]) not in (type(''), type(u'')):
+            return None
+        return self.cache[d]
 
     def getFreetext(self, url):
-        return self.freetext.get(url2registereddomain(url))
+        """
+        Calculates a dicitionary mapping country codes to the number of
+        mentions of them in the whois record. Returns None on failure, and an
+        empty dictionary if no entities are found.
+        """
+        d = url2registereddomain(url)
+        if not d:
+            return None
+        if d not in self.cache:
+            self.add_to_cache(d)
+        if d not in self.cache or type(self.cache[d]) != type({}):
+            return None
+        return self.cache[d]
+
+    def add_to_cache(self, domain):
+        raw = retrieve_whois_record(domain)
+        parsed = extract_parsed_whois_country(raw, self.countries, self.aliases)
+        if parsed:
+            self.cache[domain] = parsed
+            self.add_cache_line(domain + u'\t' + parsed + '|p')
+        else:
+            freetext = extract_freetext_whois_country(raw, self.regexes)
+            if freetext:
+                self.cache[domain] = freetext
+                pairs = [u'%s|%s' % (cc, n) for (cc, n) in freetext.items()]
+                self.add_cache_line(domain + u'\t' + u','.join(pairs))
+
+    def add_cache_line(self, line):
+        f = gp_open(self.cache_path, 'a')
+        f.write(line + u'\n')
+        f.close()
 
 class ParsedWhoisFeature:
     def __init__(self, provider=None):
-        if not provider: provider = OfflineWhoisProvider()
+        if not provider: provider = WhoisProvider()
         self.provider = provider
         self.name = 'parsed_whois'
 
@@ -59,31 +130,25 @@ class ParsedWhoisFeature:
         else:
             return (0, {})
 
+class FreetextWhoisFeature:
+    def __init__(self, provider=None):
+        if not provider: provider = WhoisProvider()
+        self.provider = provider
+        self.name = 'freetext_whois'
 
-class OnlineWhoisProvider:
-    def __init__(self, delegate=None):
-        self.delegate = delegate
-        self.aliases = {}
-        self.tld_to_countries = {}
+    def infer(self, url):
+        r = self.provider.getFreetext(url)
+        if r:
+            return (0.60, r)
+        else:
+            return (0, {})
 
-        # for c in read_countries():
-        #     if c.tld in self.tld_to_countries:
-        #         warn('tld %s shared betweee %s and %s' % (c.tld, c.name, self.tld_to_countries[c.tld].name))
-        #     self.tld_to_countries[c.tld] = c
-
-    def getParsed(self, url):
-        d = url2registereddomain(url)
-        if self.delegate:
-            r = self.delegate.getParsed(d)
-            if r: return r
-
-    def getFreetext(self, url):
-        d = url2registereddomain(url)
-        if self.delegate:
-            r = self.delegate.getFreetext(d)
-            if r: return r
 
 def retrieve_whois_record(domain):
+    """
+    Retrieves a list of WhoIs records, each of which is a string. Most domains
+    will only have one record, but some may require recursive lookups.
+    """
     return pythonwhois.net.get_whois_raw(domain)
 
 
@@ -175,28 +240,14 @@ def read_aliases(dir=DATA_DIR):
 
     return dict(aliases)
 
-
-class FreetextWhoisFeature:
-    def __init__(self, provider=None):
-        if not provider: provider = OfflineWhoisProvider()
-        self.provider = provider
-        self.name = 'freetext_whois'
-
-    def infer(self, url):
-        r = self.provider.getFreetext(url)
-        if r:
-            return (0.60, r)
-        else:
-            return (0, {})
-
-def test_parsed_offline_whois():
-    provider = OfflineWhoisProvider()
+def test_parsed_provider():
+    provider = WhoisProvider()
     assert(not provider.getParsed('foo'))
     assert(provider.getParsed('http://www.unesco.org/foo/bar') == 'fr')
     assert(provider.getParsed('http://budapestbylocals.com/foo/bar') == 'hu')
 
-def test_freetext_offline_whois():
-    provider = OfflineWhoisProvider()
+def test_freetext_provider():
+    provider = WhoisProvider()
     assert(provider.getFreetext('http://foo.google.ca/foo/bar'), {'us' : 0.5, 'ca' : 0.5})
 
 def test_online_whois():
@@ -214,5 +265,5 @@ def test_freetext_whois():
     # print '\n'.join(records)
     regexes = build_regexes(aliases)
     freetext = extract_freetext_whois_country(records, regexes)
-    assert(freetext == None)
+    assert(freetext == {'us' : 3})
 
